db9796a2837689ed44bf9fb06adaef5d12a6ab11
adityajewaria108 <97654075+adityajewaria108@users.noreply.github.com>
2022-01-14 00:15:32 +0530

    Add files via upload

---
+++ HTML.c
@@ -0,0 +1,35 @@
+<IDOCTYPE html>
+<html>
+    <head>
+        <title> first assignment </title>
+</head>
+<body>
+<h1> Assignment on List </H1>
+<h2> Things I Love to do </h2>
+<ol>
+    <li>Eating</li>
+    <li>Travelling</li>
+    <li>Developing</li>
+</ol>
+     <h2> Web Development </h2>
+<ul>
+    <li> Frontend
+        <UL>
+            <li> HTML</li>
+            <li> CSS</li>
+            <li> java script </li>
+            <li> React </li>
+        </ul> </li>
+</ul>
+<ul><li> backbend
+    <ol>
+        <li> Node Js </li>
+        <li> Express Js </li>
+    </ol>
+  </li> </ul>
+  <ul> <li> Database
+      <ul> <li>MongDW</li> </ul>
+      </li></ul>
+      </body>
+      </html>
+
